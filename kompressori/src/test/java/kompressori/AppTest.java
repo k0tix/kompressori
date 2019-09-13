cdce6e82b20dfdfc887804351d651659293c28d5
k0tix <otto.pirttimaa@gmail.com>
2019-09-14 01:49:38 +0300

    Add tests for huffman functionality

---
+++ kompressori/src/test/java/kompressori/AppTest.java
@@ -0,0 +1,18 @@
+package kompressori;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
